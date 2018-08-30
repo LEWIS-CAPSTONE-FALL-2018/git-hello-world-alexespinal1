1ca3fd91c7c8665f42a23f4b605cedbef5f0acdc
alexespinal1 <35497719+alexespinal1@users.noreply.github.com>
2018-08-30 11:25:17 -0500

    This is my Hello World python program

---
+++ Assingment1.py
@@ -0,0 +1,10 @@
+#Alex Espinal 
+# This is my Hello World assingment using python as my language of choice.
+
+print("This is my Hello World")
+print("\n")
+#Used a for loop to print the list of strings that content different Hello Worlds
+hellos = ['Hello World!' , 'Hello Worlds!!!' , 'Hello Worlds to everyone' , 'Hello to the World']
+#This for loop will print the list of string 4 times 
+for hello in hellos:
+    print(hellos)
